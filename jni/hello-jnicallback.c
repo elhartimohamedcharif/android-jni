90713fcbaf908e5671e7a56bd2bd1c9ed3b82347
EL HARTI Mohamed Charif <mohamed.charif.harti@gmail.com>
2018-11-08 15:52:57 +0000

    use .so library

--- jni/hello-jnicallback.c
+++
@@ -1,8 +0,0 @@
-
-#include <jni.h>
-
-JNIEXPORT jstring JNICALL
-Java_com_example_hellojnicallback_MainActivity_stringFromJNI(JNIEnv* env, jobject thiz)
-{
-    return (*env)->NewStringUTF(env, "Hello World");
-}
